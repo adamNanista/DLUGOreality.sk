fd6f5eedaa65b355caa3524bdfcbf741ffec7511
adamNanista <adamko.nanista1111@gmail.com>
2021-04-20 19:39:31 +0200

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,38 @@
 (function () {
+	
+	// Fotky - Slider 
+	
+	var primarySlider = new Splide( '#photo__slider--primary', {
+		type: 'fade', 
+		heightRatio: 0.67, 
+		cover: true, 
+		pagination: false, 
+	} );
+	
+	var secondarySlider = new Splide( '#photo__slider--secondary', {
+		fixedWidth: 100, 
+		height: 67, 
+		gap: 8, 
+		rewind: true, 
+		cover: true, 
+		isNavigation: true,
+		arrows: false, 
+		pagination: false, 
+		breakpoints: {
+			1024: {
+				gap: 4, 
+			},
+			767: {
+				fixedWidth: 66, 
+				height: 44, 
+			},
+		},
+	} ).mount();
+	
+	primarySlider.sync( secondarySlider ).mount();
+	
+	// Recenzie - Slider
+	
 	new Splide( '.testimonials__slider', {
 		type: 'loop', 
 		perPage: 2, 
@@ -11,4 +45,5 @@
 			},
 		},
 	} ).mount();
+	
 })();
