656729c5ed693a19e6e72a8345b1fa4ee52155fd
adamNanista <adamko.nanista1111@gmail.com>
2021-04-21 19:25:41 +0200

    Add files via upload

--- assets/js/map.js
+++ assets/js/map.js
@@ -8,7 +8,7 @@ function mapDialog(gpsX,gpsY,address) {
 	//#lang je vo sablone module.copyright.html daneho webu (lang posielany z kontrolera v cmsV3 -> modCopyright)
 	if($("#currentLang").data("lang") == "ro_RO") kdeSaNachadza = 'Unde este situatÄ proprietatea?';
 
-	var canvas='map_canvas'+Math.random();
+	/*var canvas='map_canvas'+Math.random();
 	var dialog='dialog'+Math.random();
 	$('<div id="'+dialog+'"><div id="'+canvas+'" style="width:500px;height:500px;"></div></div>').dialog({
 		open:function(){drawMap(gpsX,gpsY,address,canvas);},
@@ -17,32 +17,27 @@ function mapDialog(gpsX,gpsY,address) {
 		height:555,
 		title:kdeSaNachadza,
 		resizable: false
-	});
+	});*/
+	
+	var canvas = document.querySelector('#map');
+	
+	if (typeof(canvas) != 'undefined' && canvas != null) {
+		drawMap(gpsX,gpsY,address,canvas);
+	}
+	
 }
 
 function drawMap(gpsX,gpsY,address,canvas) {
 
-	gpsX=gpsX.toString().replace(",",".");
-	gpsY=gpsY.toString().replace(",",".");
+	gpsX=gpsX.replace(",",".");
+	gpsY=gpsY.replace(",",".");
 	
-	var mymap = L.map(canvas).setView([gpsX, -gpsY], 13);
-
-	/*map = new OpenLayers.Map(canvas);
-    map.addLayer(new OpenLayers.Layer.OSM());
-
-    var lonLat = new OpenLayers.LonLat( gpsY, gpsX )
-          .transform(
-            new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
-            map.getProjectionObject() // to Spherical Mercator Projection
-          );
-          
-    var zoom=16;
-
-    var markers = new OpenLayers.Layer.Markers( "Markers" );
-    map.addLayer(markers);
-    
-    markers.addMarker(new OpenLayers.Marker(lonLat));
-    
-    map.setCenter (lonLat, zoom);*/
+	var mymap = L.map(canvas).setView([gpsX, gpsY], 16);
+	
+	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(mymap);
+	
+	L.marker([gpsX, gpsY]).addTo(mymap);
     
 }
